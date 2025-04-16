0a725e67bbf7753af286fc0b2206612662d13bdf
Ballast <birgit.leitner@yahoo.de>
2025-04-16 17:54:57 +0200

    scaling the chickens

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -3,5 +3,9 @@ class Chicken extends MovableObject{
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 500;
+        this.y = 370;
+        this.speed = 2;
+        this.height = 50;
+        this.width = 50;
     }
 }
