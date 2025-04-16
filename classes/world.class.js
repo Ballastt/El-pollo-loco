3c578cddf3b38806751b45171911fdbe2ed6122f
Ballast <birgit.leitner@yahoo.de>
2025-04-16 17:55:23 +0200

    adding background objects

--- classes/world.class.js
+++ classes/world.class.js
@@ -8,6 +8,14 @@ class World {
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+        
+    ];
+
     canvas;
     ctx;
 
@@ -21,16 +29,11 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
-
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
-
-
+        this.addObjectToMap(this.backgroundObjects);
+        this.addObjectToMap(this.clouds);
+        this.addToMap(this.character);
+        this.addObjectToMap(this.enemies);
+        
 
         //Draw() wird immer wieder aufgerufen
         let self = this;
@@ -38,4 +41,14 @@ class World {
             self.draw();
         }
     )};
+
+    addObjectToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    }
 }
