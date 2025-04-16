2f099718bc154494f83ba0ad8b56ca6fb45cabbc
Ballast <birgit.leitner@yahoo.de>
2025-04-16 17:55:11 +0200

    scaling the clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,7 +2,10 @@ class Cloud extends MovableObject{
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
     
-        this.x = 200 + Math.random() * 500;
+        this.x = 100 + Math.random() * 500;
+        this.y = 20;
+        this.width = 500;
+        this.height = 400;
         
     }
 }
