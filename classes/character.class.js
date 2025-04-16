3eaa5dacbbe6a5be6326adde89cc29273b0f735a
Ballast <birgit.leitner@yahoo.de>
2025-04-16 17:54:36 +0200

    scaling the character

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,6 +3,11 @@ class Character extends MovableObject{
     
     constructor(){
         super().loadImage('/img/2_character_pepe/2_walk/W-21.png');
+
+        this.x = 50;
+        this.y = 180;
+        this.width = 120;
+        this.height = 250;
     }
     
     jump() {
