6b2225450813f2b63f604a5ae61f43cc4963bf5f
Ballast <birgit.leitner@yahoo.de>
2025-04-17 12:22:32 +0200

    variables to cache images and array added

--- classes/moveable-objects.class.js
+++ classes/moveable-objects.class.js
@@ -4,12 +4,30 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
 
     //loadImage()
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ....]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image(); //1. neues Image-Objekt wird erzeugt
+            img.src = path;     // 1. Bildpfad wird gesetzt => Browser lädt das Bild
+            this.imageCache[path] = img; //3. Bild wird in Cache gespeichert
+        });
+      
+    }
+      
+    
 
     //Schablone (wie ein JSON)
     moveRight(){
