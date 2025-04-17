96eac4bc4d73a48fd8f8264623777cf4552523b2
Ballast <birgit.leitner@yahoo.de>
2025-04-17 12:21:36 +0200

    IMAGES_WALKING + loadImages + setIntervall

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,4 +1,11 @@
 class Chicken extends MovableObject{
+
+    IMAGES_WALKING = [
+       'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+       'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+       'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
@@ -7,5 +14,21 @@ class Chicken extends MovableObject{
         this.speed = 2;
         this.height = 50;
         this.width = 50;
+
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+
+        
+    }
+
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 400);
+        
     }
 }
