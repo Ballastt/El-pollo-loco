f641407c08c3f2a053515cbb5701abc0a3efdc11
Ballast <birgit.leitner@yahoo.de>
2025-04-22 18:27:47 +0200

    typo + updated animate and new function in setInterval

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,4 +1,4 @@
-class Character extends MovableObject{
+class Character extends MoveableObject{
     IMAGES_WALKING = [
         '/img/2_character_pepe/2_walk/W-21.png',
         '/img/2_character_pepe/2_walk/W-22.png',
@@ -29,35 +29,27 @@ class Character extends MovableObject{
     animate() {
 
         setInterval(() => {
-            if(this.world.keyboard.RIGHT) {
+            if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
-      
             }
 
-            if(this.world.keyboard.LEFT) {
+            if(this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
-       
             }
-            this.world.camera_x = -this.x;
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 60)
 
 
         setInterval(() => {
             if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
             //Walk animation
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
 
-
-    
-    
     jump() {
 
     }
