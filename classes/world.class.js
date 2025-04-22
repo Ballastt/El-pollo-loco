8fdb1fde70d9a8dddaecfec691ed0d8d9fb29515
Ballast <birgit.leitner@yahoo.de>
2025-04-22 12:19:25 +0200

    code to turn camera view in draw(); variables with reference to array in level1

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,24 +1,13 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
-        
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
 
     canvas;
     ctx;
     keyboard;
+    camera_x = 0; //Kameradrehung
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
@@ -36,10 +25,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0); //Kameradrehung inital
+
         this.addObjectToMap(this.backgroundObjects);
         this.addObjectToMap(this.clouds);
         this.addToMap(this.character);
         this.addObjectToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0); //nach dem malen der Objekte, müssen wir wieder zurücksetzen, damit es sich nicht weiter dreht
         
 
         //Draw() wird immer wieder aufgerufen
