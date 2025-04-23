3939f434ee6f716b6b086d922b949f05dac9b4b1
Ballast <birgit.leitner@yahoo.de>
2025-04-23 10:09:03 +0200

    new class

---
+++ classes/small-chicken.class.js
@@ -0,0 +1,14 @@
+class SmallChicken extends Chicken {
+    constructor() {
+        super(
+            'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+            [
+                'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+                'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+                'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
+            ],
+            35, 35, [0.2, 0.2]
+        );
+        this.y = 385;
+    }
+}
