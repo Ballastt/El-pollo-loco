78492e548e3ba33264b70dc0ca9b688b930c187f
Ballast <birgit.leitner@yahoo.de>
2025-04-23 10:09:18 +0200

    new class

---
+++ classes/normal-chicken.class.js
@@ -0,0 +1,13 @@
+class NormalChicken extends Chicken {
+    constructor() {
+       super(
+            'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+            [
+                'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+                'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+                'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+            ],
+        50, 50, [0.25, 0.25]
+       );
+    }
+}
