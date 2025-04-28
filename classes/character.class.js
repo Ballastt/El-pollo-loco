7e6c4cae1a7d44b4d33fd41f71190cc37824160d
Ballast <birgit.leitner@yahoo.de>
2025-04-28 19:35:27 +0200

    images jumping added

--- classes/character.class.js
+++ classes/character.class.js
@@ -8,13 +8,25 @@ class Character extends MoveableObject{
         '/img/2_character_pepe/2_walk/W-26.png'
     ];
 
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png'
+    ];
+
     world;
     
     constructor(){
         super().loadImage('/img/2_character_pepe/2_walk/W-21.png');
 
         this.x = 50;
-        this.y = 180;
+        this.y = 80;
         this.width = 120;
         this.height = 250;
         this.speed = 6;
@@ -23,6 +35,8 @@ class Character extends MoveableObject{
         this.walkingSound.loop = true;
 
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applyGravity();
         this.animate();
     }
 
@@ -50,14 +64,25 @@ class Character extends MoveableObject{
                 this.walkingSound.pause();
                 this.walkingSound.currentTime = 0;
             }
+
+            
+
+            if (this.world.keyboard.UP) {
+                this.speedY = 20;
+            }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60)
 
 
         setInterval(() => {
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            //Walk animation
-            this.playAnimation(this.IMAGES_WALKING);
+            
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                //Walk animation
+                this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
     }
