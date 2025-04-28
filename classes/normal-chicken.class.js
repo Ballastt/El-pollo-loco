5852732457ac4faa5a0b421047549c508189574b
Ballast <birgit.leitner@yahoo.de>
2025-04-28 16:11:30 +0200

    new speed for chickens

--- classes/normal-chicken.class.js
+++ classes/normal-chicken.class.js
@@ -7,7 +7,7 @@ class NormalChicken extends Chicken {
                 'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
                 'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
             ],
-        50, 50, [0.55, 0.75]
+        50, 50, [0.35, 0.65]
        );
     }
 }
