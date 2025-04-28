087f6e63d1be86b6ee8b4378336e130b334a1aac
Ballast <birgit.leitner@yahoo.de>
2025-04-28 13:30:21 +0200

    generating clouds

--- levels/level1.js
+++ levels/level1.js
@@ -14,8 +14,10 @@ const layerSets = [
     ],
 ];
 
-for (let i = -1; i <= 5; i++) {
-    const position = i * 719;
+//Calculate layers needed to fill level width
+const numberOfBackgrounds = Math.ceil(6500 / 720);
+for (let i = -1; i < numberOfBackgrounds; i++) {
+    const position = i * 719; //repeating the layer
     const layerSet = layerSets[i % 2 === 0 ? 1 : 0]; // Alternate between sets
     layerSet.forEach(layer => {
         backgroundObjects.push(new BackgroundObject(layer, position));
@@ -43,10 +45,27 @@ function generateEnemies() {
     return enemies;
 }
 
+
+function generateClouds() {
+    const clouds = [];
+
+    const cloudDistance = 660; // 800px apart
+    const numberOfClouds = Math.floor(7000 / cloudDistance);
+
+    for (let i = 0; i <= numberOfClouds; i++) {
+        const cloud = new Cloud();
+        cloud.x = i * cloudDistance + Math.random() * 20; // only small random
+        cloud.y = 0 + Math.random() * 30; // between 30 and 70px (higher sky)
+        cloud.speed = 1.1 + Math.random() * 0.35;
+        clouds.push(cloud);
+    }
+
+    return clouds;
+}
+
+
 const level1 = new Level(
     generateEnemies(),
-    [
-        new Cloud()
-    ],
+    generateClouds(),
     backgroundObjects
 );
