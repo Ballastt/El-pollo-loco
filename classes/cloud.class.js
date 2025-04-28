42d0391ad2c05efb80a716374642d09ed9958902
Ballast <birgit.leitner@yahoo.de>
2025-04-28 13:29:28 +0200

    clouds update in background

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -4,11 +4,8 @@ class Cloud extends MoveableObject{
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
     
-        this.x = 100 + Math.random() * 500;
-        this.y = 20;
         this.width = 600;
         this.height = 400;
-        this.speed = 1.2;
         this.animate();
         
         
@@ -16,6 +13,11 @@ class Cloud extends MoveableObject{
 
     animate() {
         this.moveLeft();
+
+        if(this.x + this.width < 0) {
+            this.x = 6000;
+            this.y = 0 + Math.random() * 40;
+        }
     }
 
     
