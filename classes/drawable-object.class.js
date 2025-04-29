8684c380acb7863945ec3de79b0fece7d5e48003
Ballast <birgit.leitner@yahoo.de>
2025-04-29 15:33:25 +0200

    drawing Hitbox for instances of

--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -3,4 +3,17 @@ class DrawableObject {
     draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
+
+    drawHitbox(ctx) {
+
+        if (this instanceof Character || this instanceof Chicken || this instanceof Coin || this instanceof Endboss) {
+            //Debug Hitbox
+            ctx.beginPath();
+            ctx.lineWidth = '4';
+            ctx.strokeStyle = 'red';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+            
+    }
 }
