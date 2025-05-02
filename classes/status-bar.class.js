a20023fc3858330a0be4edc6a3ec2b76f6394cdf
Ballast <birgit.leitner@yahoo.de>
2025-05-02 17:30:16 +0200

    array kept general, isReversed, resolveImageIndex update

--- classes/status-bar.class.js
+++ classes/status-bar.class.js
@@ -1,48 +1,50 @@
 class StatusBar extends DrawableObject{
+    images = []; // Bilder für diese StatusBar
+    percentage = 100; // Standardwert für abwärtszählende Statusbars
+    isReversed = false; // Legt fest, ob die Logik für die CoinBar umgekehrt ist
 
-    IMAGES_HEALTH = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
-    ];
-
-    percentage = 100;
-
-    constructor(x, y, width, height) {
+    constructor(images, x, y, width, height, isReversed = false) {
         super(); //ruft den Konstruktor der drawable Klasse auf
-        this.loadImages(this.IMAGES_HEALTH);
+        this.images = images;
+        this.loadImages(this.images);
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.reversed = isReversed;
 
         setTimeout(() => {
-            this.setPercentage(100);
+            this.setPercentage(isReversed ? 0 : 100);
         }, 50); // 50ms reichen oft aus
     }
 
     setPercentage(percentage) {
         this.percentage = percentage;
-        let path = this.IMAGES_HEALTH[this.resolveImageIndex()];
+        let path = this.images[this.resolveImageIndex()];
         this.img = this.imageCache[path];
-        console.log(path)
+        if (!this.img) {
+            console.error(`Image not found for percentage: ${percentage}, path: ${path}`);
+        } else {
+            console.log(`StatusBar image set for ${percentage}%: ${path}`);
+        }
     }
     
     resolveImageIndex() {
-        if (this.percentage == 100) {
-            return 5;
-        } else if (this.percentage > 80) {
-            return 4;
-        } else if (this.percentage > 60) {
-            return 3;
-        } else if (this.percentage > 40) {
-            return 2;
-        } else if (this.percentage > 20){
-            return 1;
+        if (this.isReversed) {
+            // Logik für aufwärtszählende StatusBar (CoinBar)
+            if (this.percentage >= 100) return 5;
+            if (this.percentage > 80) return 4;
+            if (this.percentage > 60) return 3;
+            if (this.percentage > 40) return 2;
+            if (this.percentage > 20) return 1;
+            return 0;
         } else {
+            // Standard-Logik (abwärtszählende StatusBars)
+            if (this.percentage == 100) return 5;
+            if (this.percentage > 80) return 4;
+            if (this.percentage > 60) return 3;
+            if (this.percentage > 40) return 2;
+            if (this.percentage > 20) return 1;
             return 0;
         }
     }
