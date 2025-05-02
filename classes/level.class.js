675fc0b3f960ea6741d1478577abc93d247cbb17
Ballast <birgit.leitner@yahoo.de>
2025-05-02 17:28:45 +0200

    total_coins logic

--- classes/level.class.js
+++ classes/level.class.js
@@ -4,11 +4,15 @@ class Level {
     backgroundObjects;
     coins;
     level_end_x = 6000;
+    totalCoins = 0;
 
     constructor(enemies, clouds, backgroundObjects, coins) {
         this.enemies = enemies;
         this.clouds = clouds;
         this.backgroundObjects = backgroundObjects;
         this.coins = coins;
+
+        // Setze das Maximum der Coins basierend auf der Anzahl der Coins im Level
+        this.totalCoins = coins.length;
     }
 }
