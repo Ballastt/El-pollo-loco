3404a1f6ca57bd86fc27b5a86ba2c0d00f75c44b
Ballast <birgit.leitner@yahoo.de>
2025-05-02 17:32:01 +0200

    new variables; audio related update; status bar updates;

--- classes/world.class.js
+++ classes/world.class.js
@@ -7,8 +7,16 @@ class World {
     camera_x = 0; //Kameradrehung
     level_end_x = 6000;
     collectedCoins = 0;
-    statusbar;
+
     healthbar;
+    throwbar;
+    coinBar;
+
+    coinCollectSound;
+    soundVolume = 0.2;
+
+    bottles = [];
+    availableBottles = [];
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
@@ -16,13 +24,66 @@ class World {
         this.keyboard = keyboard;
 
         this.character = new Character(this);
-        this.statusbar = new StatusBar(0, 0, 250, 60);
+
+        for(let i = 0; i < 30; i++) {
+            this.availableBottles.push(new SalsaBottle(0, 0));
+        }
+
+        this.coinCollectSound = new Audio('audio/get_coin.mp3');
+        this.updateSoundVolume();
+
+         // Bildpfade für die drei Statusbars definieren
+         const healthImages = [
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+            'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
+        ];
+
+        const throwImages = [
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+            'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
+        ];
+
+        const coinImages = [
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+            'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
+        ];
+        
+        // Statusbars mit den jeweiligen Bildern initialisieren
+        this.healthBar = new StatusBar(healthImages, 0, 0, 250, 60);
+        this.throwBar = new StatusBar(throwImages, 0, 50, 250, 60);
+        this.coinBar = new StatusBar(coinImages, 0, 100, 250, 60, true);
+
+        this.coinBar.setPercentage(0);
        
         this.draw();
         this.checkCollisions();
     }
 
-
+    throwBottle() {
+        if (this.availableBottles.length > 0) {
+            // Flasche aus dem Array "popen"
+            const bottle = this.availableBottles.pop();
+            bottle.x = this.character.x + 50; // Position der Flasche vor dem Charakter
+            bottle.y = this.character.y;
+            bottle.throwBottle(); // Flasche werfen
+            this.bottles.push(bottle); // Flasche in das "geworfene"-Array verschieben
+        } else {
+            console.log('Keine Flaschen mehr verfügbar!');
+        }
+    }
+    
     checkCollisions() {
         setInterval(() => {
             this.character.checkCollisionsWithEnemy(this.level.enemies);
@@ -44,6 +105,11 @@ class World {
 
         this.ctx.translate(this.camera_x, 0); //Kameradrehung inital
 
+        // Geworfene Flaschen zeichnen
+        this.bottles.forEach(bottle => {
+            this.addToMap(bottle);
+        });
+
         //alle "beweglichen" Objekte zeichnen
         this.addObjectToMap(this.level.backgroundObjects);
         this.addObjectToMap(this.level.coins);
@@ -55,7 +121,11 @@ class World {
         this.ctx.translate(-this.camera_x, 0); //nach dem malen der Objekte, müssen wir wieder zurücksetzen, damit es sich nicht weiter dreht
         
         //Zeichne UI-elemente die nicht von der Kamera beeinflusst sind
-        this.addToMap(this.statusbar);
+        // Zeichne die Statusbars
+        this.addToMap(this.healthBar);
+        this.addToMap(this.throwBar);
+        this.addToMap(this.coinBar);
+
 
          // Nächster Frame
         requestAnimationFrame(() => this.draw());
@@ -95,8 +165,24 @@ class World {
             if (this.character.isColliding(coin)){
                 this.level.coins.splice(index, 1); //removing coin from array
                 this.collectedCoins++;
-                console.log(`Coins collected: ${this.collectedCoins}`)
+
+                //Audio abspielen
+                this.coinCollectSound.currentTime = 0;
+                this.coinCollectSound.play();
+
+                console.log(`Coins collected: ${this.collectedCoins} / ${this.level.totalCoins}`);
+
+                 // Aktualisiere die CoinBar
+                if (this.coinBar) {
+                    let percentage = (this.collectedCoins / this.level.totalCoins) * 100;
+                    this.coinBar.setPercentage(percentage);
+                }
             }
         });
+    }
+
+    updateSoundVolume() {
+        // Lautstärke für alle Sounds anpassen
+        this.coinCollectSound.volume = this.soundVolume;
     }
 }
