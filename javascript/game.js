b1021db825f293913dcc99c782cb16239f0a4748
Ballast <birgit.leitner@yahoo.de>
2025-05-03 13:52:52 +0200

    some logs out + new key "d"

--- javascript/game.js
+++ javascript/game.js
@@ -17,45 +17,45 @@ function init() {
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowRight') {
         keyboard.RIGHT = true;
-        console.log('RIGHT pressed', keyboard);
     }
     if (e.key === 'ArrowLeft') {
         keyboard.LEFT = true;
-        console.log('LEFT pressed', keyboard);
     }
     if (e.key === 'ArrowUp') {
         keyboard.UP = true;
-        console.log('UP pressed', keyboard);
     }
     if (e.key === 'ArrowDown') {
         keyboard.DOWN = true;
-        console.log('DOWN pressed', keyboard);
     }
     if (e.key === ' ') {
         keyboard.SPACE = true;
         console.log('SPACE pressed', keyboard);
+    }
+    if (e.key === 'd' || e.key === 'D') { // Taste D hinzufügen
+        keyboard.D = true;
+        console.log('D pressed', keyboard);
     }
 });
 
 window.addEventListener('keyup', (e) => {
     if (e.key === 'ArrowRight') {
         keyboard.RIGHT = false;
-        console.log('RIGHT released', keyboard);
     }
     if (e.key === 'ArrowLeft') {
         keyboard.LEFT = false;
-        console.log('LEFT released', keyboard);
     }
     if (e.key === 'ArrowUp') {
         keyboard.UP = false;
-        console.log('UP released', keyboard);
     }
     if (e.key === 'ArrowDown') {
         keyboard.DOWN = false;
-        console.log('DOWN released', keyboard);
     }
     if (e.key === ' ') {
         keyboard.SPACE = false;
         console.log('SPACE released', keyboard);
+    }
+    if (e.key === 'd' || e.key === 'D') { // Taste D hinzufügen
+        keyboard.D = false;
+        console.log('D released', keyboard);
     }
 });
