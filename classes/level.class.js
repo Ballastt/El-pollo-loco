f4e947a0c33e5b649a577d93df54602608aae402
Ballast <birgit.leitner@yahoo.de>
2025-05-04 11:47:43 +0200

    bottles added

--- classes/level.class.js
+++ classes/level.class.js
@@ -3,14 +3,16 @@ class Level {
     clouds;
     backgroundObjects;
     coins;
+    bottles;
     level_end_x = 6000;
     totalCoins = 0;
 
-    constructor(enemies, clouds, backgroundObjects, coins) {
+    constructor(enemies, clouds, backgroundObjects, coins, bottles) {
         this.enemies = enemies;
         this.clouds = clouds;
         this.backgroundObjects = backgroundObjects;
         this.coins = coins;
+        this.bottles = bottles;
 
         // Setze das Maximum der Coins basierend auf der Anzahl der Coins im Level
         this.totalCoins = coins.length;
