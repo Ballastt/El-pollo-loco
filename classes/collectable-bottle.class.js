2aa2a2fcb7325b38999d1146590567ee664f41b7
Ballast <birgit.leitner@yahoo.de>
2025-05-04 13:21:13 +0200

    hitbox in the class

--- classes/collectable-bottle.class.js
+++ classes/collectable-bottle.class.js
@@ -1,6 +1,15 @@
 class CollectableBottle extends CollectableObject {
     constructor(x, y) {
-        super(x, y, 50, 50); // Position und Größe der Flasche
+        super(x, y, 70, 50); // Position und Größe der Flasche
         this.loadImage('img/6_salsa_bottle/salsa_bottle.png'); // Lade das Bild der Flasche
+
+        this.hitbox = {
+            offsetX: 23, //Verschiebung der Hitbox nach rechts
+            offsetY: 6, //Verschiebung der Hitbox nach unten
+            width: 25, 
+            height: 40
+        }
     }
+
+    
 }
