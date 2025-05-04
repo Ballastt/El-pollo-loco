7bd390b4bd305016e096d9810e228bfd46219ae2
Ballast <birgit.leitner@yahoo.de>
2025-05-04 11:50:16 +0200

    class update

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,82 +1,31 @@
 class ThrowableObject extends MoveableObject {
-    IMAGES_ROTATION = [
-        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
-    ];
-
-    IMAGES_SPLASH = [
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
-        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
-    ];
-
-    IMAGES_GROUND = [
-        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
-        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
-    ];
-
-    constructor(x, y) {
-       super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
-       this.loadImages(this.IMAGES_ROTATION);
-       this.loadImages(this.IMAGES_SPLASH);
-       this.loadImages(this.IMAGES_GROUND);
-  
-       this.x = x;
-       this.y = y;
-       this.width = 70; // Beispielgröße
-       this.height = 70; // Beispielgröße
-
-       this.isFlying = false;
-       this.throw();
+    constructor(x, y, width, height) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.isFlying = false;
     }
 
     throw() {
         this.isFlying = true;
-        this.speedY = 30; //Beispielgeschwindigkeit
-        this.applyGravity();
-        setInterval(() => {
-            this.x += 8;
-            
+        this.speedX = this.otherDirection ? -10 : 10;
+        this.speedY = -20;
+
+        // Intervall für die Bewegungslogik
+        this.throwInterval = setInterval(() => {
+            if (this.isFlying) {
+                this.x += this.speedX;
+                this.y += this.speedY;
+                this.applyGravity(); // Schwerkraft anwenden
+            }
         }, 25);
-
-        //Animation der Rotation starten
-        /*this.animateRotation();*/
     }
 
-    animateRotation() {
-        let index = 0;
-        const interval = setInterval(() => {
-            if (!this.isFlying) {
-                clearInterval(interval); //stoppt Animation, wenn flasche nicht mehr fliegt
-                return;
-            }
-            this.img = this.imageCache[this.IMAGES_ROTATION[index]];
-            index = (index + 1) % this.IMAGES_ROTATION.length;
-        }, 100);
-    }
-
-    splash() {
-        this.isFlying = false;
-
-        let index = 0;
-        const interval = setInterval(() => {
-            if (index >= this.IMAGES_SPLASH.length) {
-                clearInterval(interval);
-                this.setGroundImage();
-                return;
-            }
-            this.img = this.imageCache[this.IMAGES_SPLASH[index]];
-            index++;
-        }, 100);
-    }
-
-    setGroundImage() {
-        const index = Math.floor(Math.random() * this.IMAGES_GROUND.length); //Zufallsindex
-        this.img = this.imageCache[this.IMAGES_GROUND[index]];
+    applyGravity() {
+        this.speedY += 1; // Schwerkraft anwenden
     }
 }
