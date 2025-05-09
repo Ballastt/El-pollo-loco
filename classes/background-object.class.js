550ec10f5ce916b830216a7c9cc4c730427b55e5
Ballast <birgit.leitner@yahoo.de>
2025-05-09 18:55:31 +0200

    fix: alt+shift+F for all files

--- classes/background-object.class.js
+++ classes/background-object.class.js
@@ -1,12 +1,9 @@
 class BackgroundObject extends MoveableObject {
-
-    width = 720;
-    height = 480;
-    constructor(imagePath, x) {
-        super().loadImage(imagePath);
-        this.x = x;
-        this.y = 480 - this.height; // 480 (Höhe vom Canvas) - 100 (Höhe des Objekts)
-        
-       
-    }
+  width = 720;
+  height = 480;
+  constructor(imagePath, x) {
+    super().loadImage(imagePath);
+    this.x = x;
+    this.y = 480 - this.height; // 480 (Höhe vom Canvas) - 100 (Höhe des Objekts)
+  }
 }
