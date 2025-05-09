55e3fcff8d0d9cbe37b7732d29a8d7e4c15bc1dc
Ballast <birgit.leitner@yahoo.de>
2025-05-09 15:21:02 +0200

    fix: image array for dead state added

--- classes/normal-chicken.class.js
+++ classes/normal-chicken.class.js
@@ -7,7 +7,10 @@ class NormalChicken extends Chicken {
                 'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
                 'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
             ],
-        55, 55, [0.35, 0.65]
+            [
+                'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+            ],
+        55, [0.35, 0.65]
        );
        this.y = 362;
     }
