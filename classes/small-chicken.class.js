7d083e7de82d2afcad03b82b133e4b9e16afa852
Ballast <birgit.leitner@yahoo.de>
2025-05-09 15:21:27 +0200

    fix: image array for dead state added

--- classes/small-chicken.class.js
+++ classes/small-chicken.class.js
@@ -7,6 +7,9 @@ class SmallChicken extends Chicken {
                 'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
                 'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
             ],
+            [
+                'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+            ],
             35, 35, [0.45, 0.80]
         );
         this.y = 380;
