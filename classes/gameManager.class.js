046a17411ab113cb595fce08ba4841b6ff6b2aa3
Ballast <birgit.leitner@yahoo.de>
2025-05-12 15:40:19 +0200

    new: centralizing gamecontrol in class gameManager

--- classes/gameManager.class.js
+++ classes/gameManager.class.js
@@ -1,27 +1,55 @@
 class GameManager {
   constructor(world) {
-    this.world = world; //Referenz auf die Spielwelt
+    this.world = world; // Referenz auf die Spielwelt
     this.isGameStopped = false;
     this.gameOverScreen = document.getElementById("game-over-screen");
+    this.startScreen = document.getElementById("start-screen");
+    this.canvas = document.getElementById("canvas");
+  }
+
+  startGame() {
+    console.log("Spiel wird gestartet...");
+    // Verstecke den Startbildschirm
+    if (this.startScreen) {
+      this.startScreen.style.display = "none";
+    }
+
+    // Verstecke den Game-Over-Screen
+    if (this.gameOverScreen) {
+      this.gameOverScreen.classList.add("hidden");
+    }
+
+    // Zeige das Canvas an
+    if (this.canvas) {
+      this.canvas.style.display = "block";
+    }
+
+    // Initialisiere das Spiel
+    this.initGame();
+  }
+
+  initGame() {
+    this.world.canvas = this.canvas;
+    this.world.level = level1;
+    this.world.level.regenerateEnemies(); // Gegner neu generieren
+
+    this.world.keyboard = keyboard; // Übergabe der Tastatur
+    console.log("Spiel erfolgreich initialisiert");
+    console.log("my character is", this.world.character);
   }
 
   gameOver() {
-    //game over overlay
     console.log("Spiel ist vorbei");
-    document.getElementById("game-over-screen").classList.remove("hidden");
-    this.showGameOverScreen();
-    this.stopGame();
-  }
-
-  showGameOverScreen() {
     if (this.gameOverScreen) {
       this.gameOverScreen.classList.remove("hidden");
     }
+    this.isGameStopped = true;
+    this.stopGame();
   }
 
   stopGame() {
-    clearInterval(this.characterMovementInterval);
-    clearInterval(this.charaterAnimationInterval);
+    clearInterval(this.world.characterMovementInterval);
+    clearInterval(this.world.characterAnimationInterval);
     this.isGameStopped = true;
   }
 }
