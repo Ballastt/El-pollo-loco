7d916d58be5bd4f69ec8a66a953aa57a1d88babb
Ballast <birgit.leitner@yahoo.de>
2025-05-15 17:49:41 +0200

    initalizing initGame from level1

--- classes/gameManager.class.js
+++ classes/gameManager.class.js
@@ -2,6 +2,7 @@ class GameManager {
   constructor(world) {
     this.world = world; // Referenz auf die Spielwelt
     this.isGameRunning = false; // Standard: Spiel läuft nicht
+    this.isPaused = false;
     this.gameOverScreen = document.getElementById("game-over-screen");
     this.startScreen = document.getElementById("start-screen");
     this.canvas = document.getElementById("canvas");
@@ -9,7 +10,15 @@ class GameManager {
 
   startGame() {
     console.log("Spiel wird gestartet...");
+
+    //Level Initialisieren
+    if (!level1) {
+      initLevel();
+      console.log("Level 1 initialisiert,", level1);
+    }
+
     this.isGameRunning = true; // Spielzustand auf "gestartet" setzen
+    this.isPaused = false;
 
     if (this.startScreen) this.startScreen.style.display = "none";
     if (this.gameOverScreen) this.gameOverScreen.classList.add("hidden");
@@ -24,6 +33,17 @@ class GameManager {
     }
   }
 
+  /*pauseGame() {
+    console.log("Spiel wird angehalten...");
+    this.isPaused = !this.isPaused;
+      if (this.isPaused) {
+        console.log("Spiel ist angehalten");
+      } else {
+        console.log("Spiel läuft weiter");
+        this.world.run();
+      }
+  }*/
+  
   stopGame() {
     console.log("Spiel wird gestoppt...");
     clearInterval(this.world.characterMovementInterval);
@@ -36,7 +56,6 @@ class GameManager {
 
     this.isGameRunning = false; // Spielstatus setzen
     if (this.gameOverScreen) {
-
       console.log("Game Over Screen wird angezeigt");
       this.gameOverScreen.classList.remove("hidden");
     } else {
