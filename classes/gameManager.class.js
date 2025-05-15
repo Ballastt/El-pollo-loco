bf08fa90abfb5d1fd7a0cf1448becdbdcbb6d743
Ballast <birgit.leitner@yahoo.de>
2025-05-15 09:59:31 +0200

    refactoring methods

--- classes/gameManager.class.js
+++ classes/gameManager.class.js
@@ -1,55 +1,47 @@
 class GameManager {
   constructor(world) {
     this.world = world; // Referenz auf die Spielwelt
+    this.isGameRunning = false; // Standard: Spiel läuft nicht
     this.gameOverScreen = document.getElementById("game-over-screen");
     this.startScreen = document.getElementById("start-screen");
     this.canvas = document.getElementById("canvas");
-    this.isGameRunning = false;
   }
 
   startGame() {
     console.log("Spiel wird gestartet...");
-    // Verstecke den Startbildschirm
-    if (this.startScreen) {
-      this.startScreen.style.display = "none";
-    }
+    this.isGameRunning = true; // Spielzustand auf "gestartet" setzen
 
-    // Verstecke den Game-Over-Screen
-    if (this.gameOverScreen) {
-      this.gameOverScreen.classList.add("hidden");
-    }
+    if (this.startScreen) this.startScreen.style.display = "none";
+    if (this.gameOverScreen) this.gameOverScreen.classList.add("hidden");
 
-    // Zeige das Canvas an
-    if (this.canvas) {
-      this.canvas.style.display = "block";
-    }
+    if (this.canvas) this.canvas.style.display = "block";
 
     // Initialisiere das Spiel
-    this.initGame();
+    if (this.world) {
+      this.world.run();
+    } else {
+      console.error("World wird nicht richtig initialisiert!");
+    }
   }
 
-  initGame() {
-    this.world.canvas = this.canvas;
-    this.world.level = level1;
-    this.world.level.regenerateEnemies(); // Gegner neu generieren
-
-    this.world.keyboard = keyboard; // Übergabe der Tastatur
-    console.log("Spiel erfolgreich initialisiert");
-    console.log("my character is", this.world.character);
+  stopGame() {
+    console.log("Spiel wird gestoppt...");
+    clearInterval(this.world.characterMovementInterval);
+    clearInterval(this.world.characterAnimationInterval);
+    this.isGameRunning = false; // Spielzustand auf "gestoppt" setzen
   }
 
   gameOver() {
     console.log("Spiel ist vorbei");
-    if (this.gameOverScreen) {
-      this.gameOverScreen.classList.remove("hidden");
-    }
-    this.isGameRunning = true;
-    this.stopGame();
-  }
 
-  stopGame() {
-    clearInterval(this.world.characterMovementInterval);
-    clearInterval(this.world.characterAnimationInterval);
-    this.isGameRunning = true;
+    this.isGameRunning = false; // Spielstatus setzen
+    if (this.gameOverScreen) {
+
+      console.log("Game Over Screen wird angezeigt");
+      this.gameOverScreen.classList.remove("hidden");
+    } else {
+      console.error("Kein Game Over Screen gefunden!");
+    }
+    this.stopGame();
   }
 }
