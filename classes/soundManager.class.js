ef6ebb2c805b10caa5bfa70c109d71d2a0a51b57
Ballast <birgit.leitner@yahoo.de>
2025-05-20 13:20:48 +0200

    Fix: Only looping sounds are played on unmute

--- classes/soundManager.class.js
+++ classes/soundManager.class.js
@@ -1,66 +1,69 @@
 class SoundManager {
-    constructor() {
-        this.sounds = {};
-        this.isMuted = false;
-    }
+  constructor() {
+    this.sounds = {};
+    this.isMuted = false;
+  }
 
-    addSound(key, filePath, loop = false, volume = 1.0) {
-        const audio = new Audio(filePath);
-        audio.loop = loop;
-        audio.volume = volume;
-        this.sounds[key] = audio;
-    }
+  addSound(key, filePath, loop = false, volume = 1.0) {
+    const audio = new Audio(filePath);
+    audio.loop = loop;
+    audio.volume = volume;
+    this.sounds[key] = audio;
+  }
 
-    play(key) {
-        const sound = this.sounds[key];
-        if (sound && !this.isMuted) {
-            sound.play();
-        }
+  play(key) {
+    const sound = this.sounds[key];
+    if (sound && !this.isMuted) {
+      sound.play();
     }
+  }
 
-    pause(key) {
-        const sound = this.sounds[key];
-        if (sound) {
-            sound.pause();
-        }
+  pause(key) {
+    const sound = this.sounds[key];
+    if (sound) {
+      sound.pause();
     }
+  }
 
-    stop(key) {
-        const sound = this.sounds[key];
-        if (sound) {
-            sound.pause();
-            sound.currentTime = 0;
-        }
+  stop(key) {
+    const sound = this.sounds[key];
+    if (sound) {
+      sound.pause();
+      sound.currentTime = 0;
     }
+  }
 
-    pauseAll() {
-        for (const key in this.sounds) {
-            this.pause(key);
-        }
+  pauseAll() {
+    for (const key in this.sounds) {
+      this.pause(key);
     }
+  }
 
-    stopAll() {
-        for (const key in this.sounds) {
-            this.stop(key);
-        }
+  stopAll() {
+    for (const key in this.sounds) {
+      this.stop(key);
     }
+  }
 
-    setVolume(key, volume) {
-        const sound = this.sounds[key];
-        if (sound) {
-            sound.volume = volume;
-        }
+  setVolume(key, volume) {
+    const sound = this.sounds[key];
+    if (sound) {
+      sound.volume = volume;
     }
+  }
 
-    mute() {
-        this.isMuted = true;
-        this.pauseAll();
-    }
+  mute() {
+    this.isMuted = true;
+    this.pauseAll();
+  }
 
-    unmute() {
-        this.isMuted = false;
-        for (const key in this.sounds) {
-            this.sounds[key].play();
-        }
+  unmute() {
+    this.isMuted = false;
+    for (const key in this.sounds) {
+      const sound = this.sounds[key];
+      if (sound.loop) {
+        sound.play();
+      }
     }
+  }
 }
