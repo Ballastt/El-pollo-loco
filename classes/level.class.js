87943e97647b657d7b07c0a827b79c06fe84443b
Ballast <birgit.leitner@yahoo.de>
2025-06-02 16:41:54 +0200

    js doc added

--- classes/level.class.js
+++ classes/level.class.js
@@ -1,13 +1,39 @@
+/**
+ * Represents a game level containing enemies, clouds, background objects, coins, and bottles.
+ */
 class Level {
+  /** @type {Array} Array of enemy objects in the level */
   enemies;
+
+  /** @type {Array} Array of cloud objects in the level */
   clouds;
+
+  /** @type {Array} Array of background objects in the level */
   backgroundObjects;
+
+  /** @type {Array} Array of coin objects in the level */
   coins;
+
+  /** @type {Array} Array of bottle objects in the level */
   bottles;
+
+  /** @type {number} The horizontal position representing the end of the level */
   level_end_x = 6000;
+
+  /** @type {number} Total number of coins in the level */
   totalCoins = 0;
+
+  /** @type {number} Total number of bottles in the level */
   totalBottles = 0;
 
+  /**
+   * Creates a new Level.
+   * @param {Array} enemies - List of enemy objects.
+   * @param {Array} clouds - List of cloud objects.
+   * @param {Array} backgroundObjects - List of background objects.
+   * @param {Array} coins - List of coin objects.
+   * @param {Array} bottles - List of bottle objects.
+   */
   constructor(enemies, clouds, backgroundObjects, coins, bottles) {
     this.enemies = enemies;
     this.clouds = clouds;
@@ -20,6 +46,9 @@ class Level {
     this.totalBottles = bottles.length;
   }
 
+  /**
+   * Regenerates the enemies in the level.
+   */
   regenerateEnemies() {
     this.enemies = generateEnemies();
   }
