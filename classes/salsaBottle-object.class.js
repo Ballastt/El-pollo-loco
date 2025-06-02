a81a969e87f2d28dbb6a01ea5aa60f738ade5bbc
Ballast <birgit.leitner@yahoo.de>
2025-06-02 16:48:38 +0200

    js doc added;

--- classes/salsaBottle-object.class.js
+++ classes/salsaBottle-object.class.js
@@ -1,4 +1,12 @@
+/**
+ * Class representing a throwable salsa bottle.
+ * Extends the ThrowableObject and handles bottle rotation, splash animation, and interaction with the game world.
+ */
 class SalsaBottle extends ThrowableObject {
+  /**
+   * Images for the bottle's rotation animation.
+   * @type {string[]}
+   */
   IMAGES_ROTATION = [
     "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
     "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
@@ -6,6 +14,10 @@ class SalsaBottle extends ThrowableObject {
     "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
   ];
 
+  /**
+   * Images for the bottle splash animation.
+   * @type {string[]}
+   */
   IMAGES_SPLASH = [
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
@@ -15,15 +27,34 @@ class SalsaBottle extends ThrowableObject {
     "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
+  /**
+   * Images for the bottle resting on the ground.
+   * @type {string[]}
+   */
   IMAGES_GROUND = [
     "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
     "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
   ];
 
+  /**
+   * Static image for the bottle.
+   * @type {string[]}
+   */
   IMAGE_BOTTLE = ["img/6_salsa_bottle/salsa_bottle.png"];
 
+  /**
+   * Reference to the game world, used for accessing sound manager and other game state.
+   * @type {Object}
+   */
   world;
 
+  /**
+   * Creates a new SalsaBottle instance.
+   * @param {number} x - Initial horizontal position.
+   * @param {number} y - Initial vertical position.
+   * @param {boolean} otherDirection - If true, bottle is thrown in the opposite direction.
+   * @param {Object} world - The game world context.
+   */
   constructor(x, y, otherDirection, world) {
     super(x, y, 70, 70);
     this.world = world;
@@ -40,19 +71,33 @@ class SalsaBottle extends ThrowableObject {
     this.throw();
   }
 
+  /**
+   * Sets the game world context.
+   * @param {Object} world - The game world.
+   */
   setWorld(world) {
     this.world = world;
   }
 
+  /**
+   * Starts the splash animation and removes the bottle after animation.
+   */
   startSplashAndRemove() {
     this.splash();
   }
 
+  /**
+   * Initiates the throw behavior and starts the rotation animation.
+   */
   throw() {
     super.throw();
     this.animateRotation();
   }
 
+  /**
+   * Animates the bottle rotation while flying.
+   * Loops through rotation images until the bottle is no longer flying.
+   */
   animateRotation() {
     let index = 0;
     const interval = setInterval(() => {
@@ -65,10 +110,13 @@ class SalsaBottle extends ThrowableObject {
     }, 100);
   }
 
+  /**
+   * Handles the splash animation when the bottle hits the ground or an object.
+   * Plays splash sound and changes image sequence accordingly.
+   */
   splash() {
     this.isFlying = false;
 
-    // Sound sofort abspielen
     if (this.world?.soundManager) {
       this.world.soundManager.play("bottleSplash");
     }
@@ -87,17 +135,26 @@ class SalsaBottle extends ThrowableObject {
     }, 100);
   }
 
+  /**
+   * Plays the splash sound with a slight delay.
+   */
   playSplashSound() {
     if (this.world?.soundManager) {
       setTimeout(() => this.world.soundManager.play("bottleSplash"), 50);
     }
   }
 
+  /**
+   * Sets the bottle image to one of the ground images randomly.
+   */
   setGroundImage() {
     const index = Math.floor(Math.random() * this.IMAGES_GROUND.length);
     this.img = this.imageCache[this.IMAGES_GROUND[index]];
   }
 
+  /**
+   * Removes the bottle from the world's throwableObjects and enemies arrays.
+   */
   removeBottleFromWorld() {
     console.log("Removing bottle from world:", this);
 
@@ -105,11 +162,16 @@ class SalsaBottle extends ThrowableObject {
     this.removeFromArray(this.world?.level?.enemies, "enemies");
   }
 
+  /**
+   * Helper method to remove this bottle from a given array in the world.
+   * @param {Array} array - The array to remove from.
+   * @param {string} name - The name of the array for logging purposes.
+   */
   removeFromArray(array, name) {
     if (array) {
       const index = array.indexOf(this);
       if (index > -1) {
-        console.log(`Flasche aus world.${name} entfernt`);
+        console.log(`Bottle removed from world.${name}`);
         array.splice(index, 1);
       }
     }
