688f2ecf9ec94691ea6174d434bd35e6da991bef
Ballast <birgit.leitner@yahoo.de>
2025-06-02 16:49:35 +0200

    js doc added;

--- classes/small-chicken.class.js
+++ classes/small-chicken.class.js
@@ -1,4 +1,11 @@
+/**
+ * Class representing a small chicken enemy.
+ * Extends the Chicken class and initializes specific properties such as images, size, and position.
+ */
 class SmallChicken extends Chicken {
+  /**
+   * Creates a new SmallChicken instance with predefined images and settings.
+   */
   constructor() {
     super(
       "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
@@ -8,13 +15,17 @@ class SmallChicken extends Chicken {
         "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
       ],
       ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"],
-      35,
-      35,
-      [0.25, 0.55],
-      "smallChickenWalking",
-      "chickenDeath"
+      35,          // width
+      35,          // height
+      [0.25, 0.55],// hitbox offset scale or similar parameters
+      "smallChickenWalking", // animation state name for walking
+      "chickenDeath"         // animation state name for death
     );
 
+    /**
+     * Vertical position of the small chicken.
+     * @type {number}
+     */
     this.y = 380;
   }
 }
