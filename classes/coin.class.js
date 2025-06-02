e6bed8cefadc8d6581b35108380981135707cc45
Ballast <birgit.leitner@yahoo.de>
2025-06-02 16:36:48 +0200

    js doc added

--- classes/coin.class.js
+++ classes/coin.class.js
@@ -1,14 +1,36 @@
+/**
+ * Class representing a collectible coin with animation.
+ * Extends CollectableObject.
+ * 
+ * @extends CollectableObject
+ */
 class Coin extends CollectableObject {
+  /**
+   * Array of image paths used for the coin animation.
+   * @type {string[]}
+   */
   IMAGES_COIN = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
 
+  /**
+   * Creates a new Coin instance at the specified position.
+   * Loads the initial image, animation frames, and sets the hitbox.
+   * Starts the coin animation.
+   * 
+   * @param {number} x - The horizontal position of the coin.
+   * @param {number} y - The vertical position of the coin.
+   */
   constructor(x, y) {
-    super(x, y, 100, 100); // Übergibt Größe an CollectableObject
+    super(x, y, 100, 100); // Pass size to CollectableObject
     this.loadImage("img/8_coin/coin_1.png");
     this.loadCollectableImages(this.IMAGES_COIN);
 
+    /**
+     * Hitbox for collision detection with offset and size.
+     * @type {{offsetX: number, offsetY: number, width: number, height: number}}
+     */
     this.hitbox = {
-      offsetX: 30, //Verschiebung der Hitbox nach rechts
-      offsetY: 30, //Verschiebung der Hitbox nach unten
+      offsetX: 30, // Horizontal offset for hitbox
+      offsetY: 30, // Vertical offset for hitbox
       width: 40,
       height: 38,
     };
@@ -17,7 +39,8 @@ class Coin extends CollectableObject {
   }
 
   /**
-   * Startet die Animation der Münze
+   * Starts the animation loop for the coin.
+   * The animation cycles through the coin images every 400 milliseconds.
    */
   animate() {
     setInterval(() => {
