f35f24ca664da64cb437b16c4c380abfdcac01a0
Ballast <birgit.leitner@yahoo.de>
2025-06-02 16:53:02 +0200

    js doc added;

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,40 +1,82 @@
+/**
+ * Represents an object that can be thrown and moves with gravity.
+ * Extends MoveableObject to inherit movement and drawable features.
+ * 
+ * @extends MoveableObject
+ */
 class ThrowableObject extends MoveableObject {
+  /**
+   * Creates a throwable object with specified position and size.
+   * 
+   * @param {number} x - Initial horizontal position.
+   * @param {number} y - Initial vertical position.
+   * @param {number} width - Width of the object.
+   * @param {number} height - Height of the object.
+   */
   constructor(x, y, width, height) {
     super();
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+
+    /** @type {number} Horizontal speed */
     this.speedX = 0;
+
+    /** @type {number} Vertical speed */
     this.speedY = 0;
+
+    /** @type {boolean} Indicates whether the object is currently flying */
     this.isFlying = false;
   }
 
+  /**
+   * Applies gravity to the vertical speed of the object,
+   * increasing speedY by 1 to simulate acceleration downward.
+   */
   applyGravity() {
-    this.speedY += 1; // Schwerkraft anwenden
+    this.speedY += 1;
   }
 
+  /**
+   * Checks if the object has hit the ground.
+   * If the vertical position `y` is at or below ground level (370),
+   * the splash effect is triggered and the throwing movement stops.
+   */
   checkGroundCollision() {
     if (this.y >= 370) {
-      // 370 ist die Bodenhöhe
+      // 370 is the ground level
       this.splash();
-      clearInterval(this.throwInterval); // Stoppe die Bewegung
+      clearInterval(this.throwInterval);
     }
   }
 
+  /**
+   * Initiates the throwing movement of the object.
+   * Sets initial horizontal and vertical speeds based on direction.
+   * Starts a timer to update the position and apply gravity.
+   */
   throw() {
     this.isFlying = true;
     this.speedX = this.otherDirection ? -10 : 10;
     this.speedY = -20;
 
-    // Intervall für die Bewegungslogik
+    // Interval to update position and apply gravity every 25 ms
     this.throwInterval = setInterval(() => {
       if (this.isFlying) {
         this.x += this.speedX;
         this.y += this.speedY;
-        this.applyGravity(); // Schwerkraft anwenden
+        this.applyGravity();
         this.checkGroundCollision();
       }
     }, 25);
+  }
+
+  /**
+   * Placeholder for splash effect when the object hits the ground.
+   * Should be implemented in subclasses.
+   */
+  splash() {
+    // To be implemented in subclasses
   }
 }
