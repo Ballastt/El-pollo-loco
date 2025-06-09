86ba4d2f02ecf7971ec5b2085f1e21fcf443e4bc
Ballast <birgit.leitner@yahoo.de>
2025-06-09 18:49:48 +0200

    js doc for character-assets.js

--- javascript/character-assets.js
+++ javascript/character-assets.js
@@ -1,4 +1,12 @@
+/**
+ * An object containing all animation frame paths for the playable character "Pepe".
+ * Each property corresponds to a character state and maps to an array of image paths
+ * that represent the animation frames for that state.
+ *
+ * @type {Object.<string, string[]>}
+ */
 const CharacterAssets = {
+  /** Walking animation frames */
   WALKING: [
     "/img/2_character_pepe/2_walk/W-21.png",
     "/img/2_character_pepe/2_walk/W-22.png",
@@ -7,6 +15,8 @@ const CharacterAssets = {
     "/img/2_character_pepe/2_walk/W-25.png",
     "/img/2_character_pepe/2_walk/W-26.png",
   ],
+
+  /** Jumping animation frames */
   JUMPING: [
     "img/2_character_pepe/3_jump/J-31.png",
     "img/2_character_pepe/3_jump/J-32.png",
@@ -18,6 +28,8 @@ const CharacterAssets = {
     "img/2_character_pepe/3_jump/J-38.png",
     "img/2_character_pepe/3_jump/J-39.png",
   ],
+
+  /** Idle animation frames (short duration) */
   IDLE: [
     "img/2_character_pepe/1_idle/idle/I-1.png",
     "img/2_character_pepe/1_idle/idle/I-2.png",
@@ -30,6 +42,8 @@ const CharacterAssets = {
     "img/2_character_pepe/1_idle/idle/I-9.png",
     "img/2_character_pepe/1_idle/idle/I-10.png",
   ],
+
+  /** Long idle animation frames (when player is inactive longer) */
   LONG_IDLE: [
     "img/2_character_pepe/1_idle/long_idle/I-11.png",
     "img/2_character_pepe/1_idle/long_idle/I-12.png",
@@ -42,11 +56,15 @@ const CharacterAssets = {
     "img/2_character_pepe/1_idle/long_idle/I-19.png",
     "img/2_character_pepe/1_idle/long_idle/I-20.png",
   ],
+
+  /** Hurt animation frames (when character is damaged) */
   HURT: [
     "img/2_character_pepe/4_hurt/H-41.png",
     "img/2_character_pepe/4_hurt/H-42.png",
     "img/2_character_pepe/4_hurt/H-43.png",
   ],
+
+  /** Death animation frames */
   DEAD: [
     "img/2_character_pepe/5_dead/D-51.png",
     "img/2_character_pepe/5_dead/D-52.png",
@@ -58,12 +76,28 @@ const CharacterAssets = {
   ],
 };
 
-
+/**
+ * Enumeration of possible character states.
+ * These constants are used to control and reference the character's current animation.
+ *
+ * @enum {string}
+ */
 const CharacterStates = {
+  /** Default idle state */
   IDLE: "idle",
+
+  /** Idle state for longer inactivity */
   LONG_IDLE: "long_idle",
+
+  /** Walking or running state */
   WALKING: "walking",
+
+  /** Jumping / airborne state */
   JUMPING: "jumping",
+
+  /** Taking damage state */
   HURT: "hurting",
+
+  /** Dead / defeat state */
   DEAD: "dead",
 };
