dd9f65479e5756e9064b2e9664e3e2f15152174f
Ballast <birgit.leitner@yahoo.de>
2025-06-11 13:10:15 +0200

    missing world references added

--- classes/statusBarManager.class.js
+++ classes/statusBarManager.class.js
@@ -7,26 +7,27 @@ class StatusBarManager {
    * Updates the throw bar UI based on collected bottles.
    */
   updateThrowBar() {
-    const maxBottles = this.level.totalBottles || 30;
-    const percentage = (this.character.collectedBottles / maxBottles) * 100;
-    this.throwBar.setPercentage(percentage);
+    const maxBottles = this.world.level?.totalBottles || 30;
+    const percentage =
+      (this.world.character.collectedBottles / maxBottles) * 100;
+    this.world.throwBar.setPercentage(percentage);
   }
 
   /**
    * Updates the coin bar UI based on collected coins.
    */
   updateCoinBar() {
-    const maxCoins = this.level.totalCoins || 40;
-    const percentage = (this.character.collectedCoins / maxCoins) * 100;
-    this.coinBar.setPercentage(percentage);
+    const maxCoins = this.world.level?.totalCoins || 40;
+    const percentage = (this.world.character.collectedCoins / maxCoins) * 100;
+    this.world.coinBar.setPercentage(percentage);
   }
 
   /**
    * Updates the health bar UI based on character health.
    */
   updateHealthBar() {
-    const maxHealth = this.level.maxHealth || 100;
-    const percentage = (this.character.health / maxHealth) * 100;
-    this.healthBar.setPercentage(percentage);
+    const maxHealth = this.world.level?.maxHealth || 100;
+    const percentage = (this.world.character.health / maxHealth) * 100;
+    this.world.healthBar.setPercentage(percentage);
   }
 }
