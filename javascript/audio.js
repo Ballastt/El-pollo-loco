bc22bfd0e87f3508b71bb060d8847e4f9f5f3ae2
Ballast <birgit.leitner@yahoo.de>
2025-06-17 16:47:48 +0200

    cleaned up initializeSoundManager

--- javascript/audio.js
+++ javascript/audio.js
@@ -41,55 +41,9 @@ let isMutedInGame = false;
 function initializeSoundManager() {
   soundManager = new SoundManager();
 
-  soundManager.addSound(
-    "backgroundMusic",
-    "audio/audio_start_screen.mp3",
-    true,
-    0.2
-  );
-  soundManager.addSound(
-    "normalChickenWalking",
-    "audio/normal_chicken_walking.mp3",
-    true,
-    0.4
-  );
-  soundManager.addSound("chickenDeath", "audio/dying_chicken.mp3", false, 0.8);
-  soundManager.addSound(
-    "smallChickenWalking",
-    "audio/small_chicken_walking.mp3",
-    true,
-    0.8
-  );
-  soundManager.addSound("coinCollect", "audio/get_coin.mp3", false, 0.5);
-  soundManager.addSound("bottleCollect", "audio/get_bottle.mp3", false, 0.5);
-  soundManager.addSound(
-    "walkingSound",
-    "audio/character_walk_on_sand.mp3",
-    true,
-    1.0
-  );
-  soundManager.addSound("jumpSound", "audio/character_jumping.mp3", false, 0.8);
-  soundManager.addSound("hurtSound", "audio/pepe_hurting.mp3", false, 0.8);
-  soundManager.addSound("snoringPepe", "audio/pepe_snoring.mp3", false, 0.7);
-  soundManager.addSound("PepeDying", "audio/pepe_dying.mp3", false, 0.6);
-  soundManager.addSound(
-    "introEndboss",
-    "audio/endboss_intro_sound.mp3",
-    false,
-    1.0
-  );
-  soundManager.addSound(
-    "endbossClucking",
-    "audio/endboss_clucking.mp3",
-    false,
-    0.6
-  );
-  soundManager.addSound("endbossAngry", "audio/endboss_angry.mp3", false, 0.8);
-  soundManager.addSound("endbossHurt", "audio/endboss_hurting.mp3", false, 0.7);
-  soundManager.addSound("endbossDying", "audio/endboss_dying.mp3", false, 0.6);
-  soundManager.addSound("bottleSplash", "audio/bottle_splash.mp3", false, 0.4);
-  soundManager.addSound("gameWon", "audio/game_won.mp3", false, 0.9);
-  soundManager.addSound("gameOver", "audio/game_over.mp3", false, 0.8);
+  soundConfig.forEach(({ name, src, loop, volume }) => {
+    soundManager.addSound(name, src, loop, volume);
+  });
 
   soundManager.setVolume(localStorage.getItem("volume") || 1.0);
 }
